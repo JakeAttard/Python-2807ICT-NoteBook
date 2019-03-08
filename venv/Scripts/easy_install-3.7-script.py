056f09eb16db7fc2597bc48894824c9586b7f721
Jake Attard <jakeattard18@gmail.com>
2019-03-08 11:30:07 +1000

    Bug Fixes

--- venv/Scripts/easy_install-3.7-script.py
+++ venv/Scripts/easy_install-3.7-script.py
@@ -1,4 +1,4 @@
-#!"C:\Users\JakeC\Desktop\University\2807ICT Workshops\venv\Scripts\python.exe"
+#!C:\Users\JakeC\Desktop\PythonExamples\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'setuptools==39.1.0','console_scripts','easy_install-3.7'
 __requires__ = 'setuptools==39.1.0'
 import re
--- venv/Scripts/pip3.7-script.py
+++ venv/Scripts/pip3.7-script.py
@@ -1,4 +1,4 @@
-#!"C:\Users\JakeC\Desktop\University\2807ICT Workshops\venv\Scripts\python.exe"
+#!C:\Users\JakeC\Desktop\PythonExamples\venv\Scripts\python.exe
 # EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip3.7'
 __requires__ = 'pip==10.0.1'
 import re
