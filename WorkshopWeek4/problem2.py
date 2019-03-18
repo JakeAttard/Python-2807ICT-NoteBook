410085309c881aac589b4f96697a3dc4579113c7
Jake Attard <jakeattard18@gmail.com>
2019-03-18 23:29:11 +1000

    Updates / Changes

--- WorkshopWeek4/problem2.py
+++ WorkshopWeek4/problem2.py
@@ -1,5 +1,7 @@
 num = int(input("How many lines?"))
+row = 1
 for i in range(0,num):
-    for j in range(0,1*i+1):
+    for j in range(0,row):
         print("#", end="")
+    row+=row
     print()
---
+++ WorkshopWeek4/problem4.py
@@ -0,0 +1,9 @@
+rugbyTeam = 15
+bigBuses = 48
+smallBuses = 10
+costBigBuses = 200.00
+costSmallBuses = 95.00
+
+teams = int(input("How many teams? "))
+while(teams <= 1):
+    print("Hire ", teams, "")
