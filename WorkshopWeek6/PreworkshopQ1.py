a14dbd3a380ef13841a10ee479988b8ad78e0e9a
Jake Attard <jakeattard18@gmail.com>
2019-03-29 11:02:44 +1000

    Preworkshop Question Week 6 Added

---
+++ WorkshopWeek6/PreworkshopQ1.py
@@ -0,0 +1,26 @@
+#A
+# for i in range(10):
+#     print(i)
+
+#B
+# for i in range(10):
+#     for j in range(3):
+#         print(i * j)
+
+#C
+# for i in range(5):
+#     for j in range(i):
+#         print(i + j)
+
+#D
+# i = 0
+# while i < 10:
+#     print(i)
+#     i += 1
+
+#E
+i = 0
+while i < 10:
+    print(i)
+    if i % 2 == 0:
+        i += 1
---
+++ WorkshopWeek6/PreworkshopQ2.py
@@ -0,0 +1,20 @@
+#A
+# for i in range(5):
+#     print('*')
+
+#B
+# for i in range(5):
+#     print('*', end="")
+# print()
+
+#C
+# for i in range(5):
+#     print('*' * i)
+
+#D
+# for i in range(5):
+#     print(' ' * i, '*', sep="")
+
+#E
+# for i in range(5):
+#     print(' ' * (5 - i), '*', sep="")
