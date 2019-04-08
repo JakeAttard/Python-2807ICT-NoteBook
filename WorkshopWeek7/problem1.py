d5769f76b3f78013c1dc4405446e8e378164dc90
Jake Attard <jakeattard18@gmail.com>
2019-04-08 20:25:55 +1000

    Changes / Fixes

--- WorkshopWeek7/problem1.py
+++ WorkshopWeek7/problem1.py
@@ -1,8 +1,10 @@
-f = open("Lear.txt", "r")
+pathFile = input("File Name: ")
+openPathFile = open(pathFile)
+
 line = int(input("how many lines: "))
 while line!=0:
-  for x in f:
+  for x in openPathFile:
     print(x)
     break
   line-=1
-f.close()
+openPathFile.close()
--- WorkshopWeek7/problem2.py
+++ WorkshopWeek7/problem2.py
@@ -14,22 +14,3 @@ print("Characters:", characters)
 print("Words:", words)
 print("Lines:", lines)
 openPathFile.close()
-
-
-
-# txt = open("Lear.txt", "r")
-# words = 0
-# characters = 0
-# lines = 0
-#
-# for x in txt:
-#     for word in x.split():
-#         characters += len(word)
-#         words += 1
-#     lines += 1
-#
-#
-# print("Characters:", characters)
-# print("Words:", words)
-# print("Lines:", lines)
-# txt.close()
