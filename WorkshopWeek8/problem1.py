6c380c9dc25b82ff4b6fd54ad35c99f1062f5b46
Jake Attard <jakeattard18@gmail.com>
2019-04-11 12:48:10 +1000

    Problem 1 & 2 Complete

--- WorkshopWeek8/problem1.py
+++ WorkshopWeek8/problem1.py
@@ -1,5 +1,11 @@
-enterNumbers = input("List: ")
-minNumber = min(enterNumbers)
-maxNumber = max(enterNumbers)
-
-print("min =", minNumber + ',', "max =", maxNumber)
+while True:
+    enterNum = input("List: ")
+    n = enterNum.split(' ')
+    print(n)
+    if len(n) >= 1:
+        for i in range(len(n)):
+            lowestNumber = min(n)
+            highestNumber = max(n)
+        print('min = ' + lowestNumber, 'max = ' + highestNumber)
+    if not enterNum:
+        break
